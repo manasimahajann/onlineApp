a75c9f2fdc169e6ea90c17daa0a6195ff0e69103
Manasi Mahajan <manasi@paymentsolutionsinc.net>
2022-02-05 21:57:40 -0500

    added code to display products
    Signed-off-by: Manasi Mahajan <manasi@paymentsolutionsinc.net>

--- ReStore/client/src/App.tsx
+++ ReStore/client/src/App.tsx
@@ -1,9 +1,22 @@
- 
-
+ const products = [
+   {
+     name:'pro1', price:100.00
+   },   
+   {
+    name:'pro2', price:120.00
+   }
+ ]
 function App() {
   return (
     <div> 
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => 
+          (
+            <li key={index}>{item.name} - {item.price}</li>
+          )
+        )}
+      </ul>
     </div>
   );
 }
