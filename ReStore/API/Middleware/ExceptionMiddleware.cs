fd4b43180d72bc86810a98cdf2d35d9859d51776
Manasi Mahajan <manasi@paymentsolutionsinc.net>
2022-05-31 15:35:17 -0400

    error handling

--- ReStore/API/Middleware/ExceptionMiddleware.cs
+++ ReStore/API/Middleware/ExceptionMiddleware.cs
@@ -2,13 +2,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using API.Data;
-using API.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+ 
 namespace API.Middleware
 {
     public class ExceptionMiddleware
@@ -18,16 +15,15 @@ namespace API.Middleware
         private readonly RequestDelegate _next;
         private readonly ILogger<ExceptionMiddleware> _logger;
         private readonly IHostEnvironment _env;
-
+ 
         public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
         {
-            Env = env;
             _env = env;
             _next = next;
             _logger = logger;
         }
         
-
+ 
         public async Task InvokeAsync(HttpContext context)
         { 
                 try
@@ -43,17 +39,17 @@ namespace API.Middleware
                     var response = new ProblemDetails
                     {
                         Status = 500,
-                        Detail = _env.IsDevelopement() ? ex.StackTrace?.ToString() : null,
+                        Detail = _env.IsDevelopment() ? ex.StackTrace?.ToString() : null,
                         Title = ex.Message
                     };
                     var options= new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
-
+ 
                     var json = JsonSerializer.Serialize(response, options);
-
+ 
                     await context.Response.WriteAsync(json);
                 }
-
-
+ 
+ 
         }
     }
 }
